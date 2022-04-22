32a617a3c9d7b07024025fd10230e9c320935e4e
Anton K <antonk@MacBook-Air-Anton.local>
2022-04-22 23:18:35 +0300

    btndel

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -53,6 +53,7 @@ const Counter = (props) => {
             >
                 -
             </button>
+            <button className="btn btn-danger btn-sm m-2" onClick={props.onDelete}>Delete</button>
         </div>
     );
 };
